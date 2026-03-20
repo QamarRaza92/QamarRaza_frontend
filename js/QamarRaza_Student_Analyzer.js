50ad4af09db645ff92ffb8ffee91897cd85bcced
Qamar Raza <raza8bb805@gmail.com>
2026-03-20 17:52:43 +0530

    Create subject wise highest score

--- js/QamarRaza_Student_Analyzer.js
+++ js/QamarRaza_Student_Analyzer.js
@@ -3,10 +3,10 @@ const students = [
     name: "Qamar Raza",
     marks: [
       { subject: "Math", score: 95 },
-      { subject: "English", score: 88 },
+      { subject: "English", score: 94 },
       { subject: "Science", score: 92 },
-      { subject: "History", score: 85 },
-      { subject: "Computer", score: 98 }
+      { subject: "History", score: 70 },
+      { subject: "Computer", score: 79 }
     ],
     attendance: 95 
     // Case: Top performer (Grade A)
@@ -14,11 +14,11 @@ const students = [
   {
     name: "Rishika Sharma",
     marks: [
-      { subject: "Math", score: 45 },
-      { subject: "English", score: 52 },
-      { subject: "Science", score: 48 },
+      { subject: "Math", score: 91 },
+      { subject: "English", score: 90 },
+      { subject: "Science", score: 95 },
       { subject: "History", score: 55 },
-      { subject: "Computer", score: 50 }
+      { subject: "Computer", score: 56 }
     ],
     attendance: 80 
     // Case: Low average (Grade C)
@@ -29,7 +29,7 @@ const students = [
       { subject: "Math", score: 85 },
       { subject: "English", score: 80 },
       { subject: "Science", score: 35 }, // Fail in subject
-      { subject: "History", score: 70 },
+      { subject: "History", score: 98 },
       { subject: "Computer", score: 75 }
     ],
     attendance: 88 
@@ -63,6 +63,7 @@ const students = [
 
 function total_marks(array)
 {
+    let student_total = [null,null,null,null,null]
     for(let i=0;i<array.length;i++)
     {
         let sum=0;
@@ -70,8 +71,10 @@ function total_marks(array)
         {
             sum = sum+array[i]['marks'][j]['score']
         }
+        student_total[i] = sum
         console.log(array[i]["name"]+" Total Marks: "+sum)
     }
+    return student_total
 }
 total_marks(students)
 
@@ -93,4 +96,44 @@ function student_average(array)
         }
     }
 }
-student_average(students)
+// student_average(students)
+
+
+function subject_wise_highest_total(array) {
+    let no_of_sub = array[0].marks.length;
+
+    for (let i = 0; i < no_of_sub; i++) {
+        let highest = array[0].marks[i].score;
+        let index = 0;
+        let subjectName = array[0].marks[i].subject;
+
+        for (let j = 1; j < array.length; j++) {
+            if (array[j].marks[i].score > highest) {
+                highest = array[j].marks[i].score;
+                index = j;
+            }
+        }
+        console.log("Highest in " + subjectName + ": " + array[index].name + " (" + highest + ")");
+    }
+}
+// subject_wise_highest_total(students)
+
+
+
+function find_topper(array)
+{
+    student_total = total_marks(students)
+    //For identifying topper
+    let topper = -1;
+    let index = 0
+    for(i=0;i<student_total.length;i++)
+    {
+        if(topper<student_total[i])
+        {
+            index=i
+            topper=student_total[i]
+        }
+    }
+    console.log("Class Topper: "+array[index].name+" with "+topper+" marks")
+}
+find_topper(students)
